c8df737facc49f09fcea610fd378367478cd55cf
Xin Luigi Chen <luxchen@ucdavis.edu>
2019-03-29 16:51:35 -0700

    push data to postgres success

--- topos/test_connection.go
+++ topos/test_connection.go
@@ -28,11 +28,17 @@ func main() {
   }
   defer db.Close()
 
-  // create connection with the local postgres database
-  err = db.Ping()
+  sqlStatement := `
+    INSERT INTO ny_data (bin, cnstrct_yr, lstmoddate, lststatype, doitt_id, heightroof, feat_code,
+                          groundelev, shape_area, shape_len, base_bbl, mpluto_bbl, geomsource)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+    RETURNING id`
+
+  id := 0
+  err = db.QueryRow(sqlStatement, 11231, 222222, "Jonathan", "Calhoun", 333333, 444444, 555555, 66666, 777777, 888888, "luigi", "waluigi", "mario").Scan(&id)
   if err != nil {
     panic(err)
   }
-  
-  fmt.Println("Successfully connected!")
+
+  fmt.Println("New record ID is:", id)
 }
